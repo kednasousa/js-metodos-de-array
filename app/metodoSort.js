fabdb7597605a3402b1578226605372a059caea5
“Kedna <kednacleiasousa@hotmail.com>
2023-06-23 14:49:31 -0300

    alterações

--- app/metodoSort.js
+++ app/metodoSort.js
@@ -2,6 +2,6 @@ let btnOdenarPorPreco = document.getElementById('btnOrdenarPorPreco')
 btnOdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco)
 
 function ordenarLivrosPorPreco() {
-	let lirvosOrdenados = livros.sort((a, b) => a.preco - b.preco)
-	exibirOsLivrosNaTela(lirvosOrdenados)
+   let lirvosOrdenados = livros.sort((a, b) => a.preco - b.preco)
+   exibirOsLivrosNaTela(lirvosOrdenados)
 }
